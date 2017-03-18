0b7f52fd169c6e659b2e3acc81ef82d309758925
senel <joshuapohan93@yahoo.com>
2017-03-18 18:33:39 +0700

    update template

--- pBlog/forms.py
+++ pBlog/forms.py
@@ -2,5 +2,4 @@ from django import forms
 
 class PostForm(forms.Form):
     title = forms.CharField(max_length=200)
-    user = forms.CharField(max_length=100)
     message = forms.CharField(widget=forms.Textarea)
--- pBlog/views.py
+++ pBlog/views.py
@@ -4,23 +4,25 @@ from pBlog.forms import PostForm
 from blogPost.models import WritePost
 from django.utils import timezone
 from django.core.paginator import Paginator
+from django.contrib.auth.models import User 
 
 def writepost(request):
     if request.method == 'POST':
         request.POST = request.POST.copy()
         form = PostForm(request.POST)
         if form.is_valid():
-            form.data['title'] = 'My huge magnum dong'
-            WritePost.objects.create(date_posted=timezone.now(), blog_title=form.data['title'], blog_post=form.data['message'], post_author=form.data['user'])
+            user = 'test'
+            #form.data['title'] = 'My huge magnum dong'
+            WritePost.objects.create(date_posted=timezone.now(), blog_title=form.data['title'], blog_post=form.data['message'], post_author=user)
             return render(request,'blog_post.html',{'form':form})
     else:
-        form = PostForm(initial={'title':'enter title...','user':'enter username here...','message':'lorem ipsum....'})
+        form = PostForm(initial={'title':'enter title...','message':'lorem ipsum....'})
 
     return render(request,'blog_post.html',{'form':form})
 
 def readpost(request):
     if request.method == 'GET':
-        post_list = WritePost.objects.filter(date_posted__lte=timezone.now()).order_by('date_posted')
+        post_list = WritePost.objects.filter(date_posted__lte=timezone.now()).order_by('-date_posted')
         page = request.GET.get('page',1)
         post_paged = Paginator(post_list,3)
         try:
